e64a1d2c222bd423e504cabb897f8d0e445d4ccd
dbelinov <ddbelinov@gmail.com>
2025-11-09 20:27:39 +0200

    Add Owner model and constants; update project references and .gitignore

---
+++ FolkLink_Source/FolkLink.Common/EntityConstants/OwnerConstants.cs
@@ -0,0 +1,10 @@
+namespace FolkLink.Common.EntityConstants;
+
+public class OwnerConstants
+{
+    public const int FirstNameMaxLength = 40;
+    public const int MiddleNameMaxLength = 40;
+    public const int LastNameMaxLength = 40;
+    public const int PhoneNumberMaxLength = 15;
+    public const int EmailMaxLength = 50;
+}
---
+++ FolkLink_Source/FolkLink.Data.Models/Owner.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+using static FolkLink.Common.EntityConstants.OwnerConstants;
+
+namespace FolkLink.Data.Models;
+
+public class Owner
+{
+    public Owner()
+    {
+        Clubs = new HashSet<Club>();
+    }
+    [Required]
+    public Guid Id { get; set; }
+    [Required]
+    [MaxLength(FirstNameMaxLength)]
+    public string FirstName { get; set; }
+    [Required]
+    [MaxLength(MiddleNameMaxLength)]
+    public string MiddleName { get; set; }
+    [Required]
+    [MaxLength(LastNameMaxLength)]
+    public string LastName { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+    [Required]
+    [EmailAddress]
+    [MaxLength(EmailMaxLength)]
+    public string Email { get; set; }
+    [Required]
+    [MaxLength(PhoneNumberMaxLength)]
+    [Phone]
+    public string PhoneNumber { get; set; }
+    public ICollection<Club> Clubs { get; set; }
+}
